c22d38aa24dcf1f782c503eee32ba8193060342d
Guilherme Sales <guilhermessales11@gmail.com>
2025-07-03 20:53:03 -0300

    adicionado tipos de arquivos no anexo

--- src/app/shared/components/input-arquivos/input-arquivos.component.ts
+++ src/app/shared/components/input-arquivos/input-arquivos.component.ts
@@ -76,12 +76,21 @@ export class InputArquivosComponent implements ControlValueAccessor, OnInit {
 
   adicionarArquivos(novosArquivos: File[]): void {
     this.isEditMode = false;
+
+    // Verifica se o número total de arquivos não excede o limite de 3
     if (this.arquivos.length + novosArquivos.length > 3) {
-      this.errorMessage = 'Você pode enviar no máximo 3 arquivos PDF.';
+      this.errorMessage = 'Você pode enviar no máximo 3 arquivos.';
       return;
     }
 
     for (let arquivo of novosArquivos) {
+      // Verifica se o arquivo é do tipo permitido
+      if (!this.isValidFileType(arquivo)) {
+        this.errorMessage = 'Arquivo inválido. Aceitamos apenas PDFs, imagens (JPEG/PNG) e documentos Word.';
+        return;
+      }
+
+      // Verifica se o arquivo já não foi adicionado
       if (!this.isDuplicate(arquivo)) {
         this.arquivos.push(arquivo);
       }
@@ -90,6 +99,17 @@ export class InputArquivosComponent implements ControlValueAccessor, OnInit {
     this.onChange([...this.arquivos]);
     this.arquivosSelecionados.emit([...this.arquivos]);
     this.errorMessage = null;
+  }
+
+  private isValidFileType(file: File): boolean {
+    const allowedTypes = [
+      'application/pdf',
+      'image/jpeg',
+      'image/png',
+      'application/msword',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    ];
+    return allowedTypes.includes(file.type);
   }
 
   private isDuplicate(file: File): boolean {
