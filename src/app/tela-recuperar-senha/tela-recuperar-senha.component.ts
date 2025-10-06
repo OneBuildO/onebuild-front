83e7839d2cea550e916541f19b6237cf6c398592
DiegoRibeiroo <diego09022004@gmail.com>
2025-10-06 15:14:26 -0300

    feat: tela de recuperar senha

--- src/app/tela-recuperar-senha/tela-recuperar-senha.component.ts
+++ src/app/tela-recuperar-senha/tela-recuperar-senha.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tela-recuperar-senha',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tela-recuperar-senha.component.css']
 })
 export class TelaRecuperarSenhaComponent implements OnInit {
+  isLoading:boolean = false;
+  isSubmited: boolean = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
+
+  redirectToLogin(){
+    this.router.navigate(['/login']);
+  }
+
+  formRecuperarSenha = this.formBuilder.group({
+    email: new FormControl('', {validators: [Validators.required, Validators.email]}),
+  });
+
+  onSubmit(){
+    this.isSubmited = true;
+    this.isLoading = true;
+
+    if(this.formRecuperarSenha.invalid){
+      this.isLoading = false;
+      return;
+    }
+
+    this.isLoading = false;
+    this.isSubmited = false;
+    return;
+  }
+
 
 }
